5921e41378f3efec2c1dcc9b73426b4bfc47b5a8
mihai <g.mihai242@gmail.com>
2025-07-16 14:03:29 +0300

    final

--- src/modules/presets/presets.service.spec.ts
+++ src/modules/presets/presets.service.spec.ts
@@ -1,18 +1,40 @@
+import { PresetsService } from './presets.service';
+import { PrismaService } from '../../services/prisma.service';
+import { ScreensService } from '../screens/screens.service';
+
 describe('PresetsService', () => {
-  it('test', () => {
-    expect(true).toBe(true);
+  let presetService: PresetsService;
+  beforeAll(() => {
+    const prismaService = new PrismaService();
+    presetService = new PresetsService(
+      prismaService,
+      new ScreensService(prismaService),
+    );
+  });
+  it.only('should have 4 presets', async () => {
+    const response = await presetService.getPresetsByScreenId(1);
+    expect(response.length).toBe(4);
   });
 
-  it('test', () => {
-    expect(true).toBe(true);
+  it('should have the same screen id', async () => {
+    const response = await presetService.getPresetsByScreenId(1);
+    response.forEach((preset) => {
+      expect(preset.screenId).toBe(1);
+    });
   });
 
-  it('test', () => {
-    expect(true).toBe(true);
+  it('should be ordered', async () => {
+    const response = await presetService.getPresetsByScreenId(1);
+    response.forEach((preset, index) => {
+      expect(preset.position).toBe(index);
+    });
   });
 
-  it('test', () => {
-    expect(true).toBe(true);
+  it('should have a valid type', async () => {
+    const response = await presetService.getPresetsByScreenId(1);
+    response.forEach((preset) => {
+      expect(['TEXT', 'BANNER', 'FULL_PAGE'].includes(preset.type)).toBe(true);
+    });
   });
 
   it('test', () => {
